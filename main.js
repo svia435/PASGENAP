4a5db5ef67fb6126c04f5816bbf8237b6b99204c
svia435 <svia435@gmail.com>
2024-06-10 11:06:51 +0700

    update

---
+++ main.js
@@ -0,0 +1,91 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+  }
+  from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyCRlnLBhUHGOF7Lfg9iy_SbfK6coM_7f1U",
+  authDomain: "insan-cemerlang-6640c.firebaseapp.com",
+  projectId: "insan-cemerlang-6640c",
+  storageBucket: "insan-cemerlang-6640c.appspot.com",
+  messagingSenderId: "917464283158",
+  appId: "1:917464283158:web:3a6179cd71818d68f6dd37"
+};
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarAbsensi() {
+  const refDokumen = collection(db, "absensi");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      tanggal: dok.data().tanggal,
+      nis: dok.data().nis,
+      nama: dok.data().nama,
+      alamat: dok.data().alamat,
+      noTlpn: dok.data().noTlpn,
+      kelas: dok.data().kelas,
+      keterangan: dok.data().keterangan,
+      
+    });
+  });
+
+
+
+  return hasil;
+}
+
+export function formatAngka(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+export async function tambahAbsensi(tanggal, nis, nama, alamat, noTlpn, kelas, keterangan) {
+  try {
+    const dokRef = await addDoc(collection(db, 'absensi'), {
+      tanggal: tanggal,
+      nis: nis,
+      nama: nama,
+      alamat: alamat,
+      noTlpn: noTlpn,
+      kelas: kelas,
+      keterangan: keterangan
+    });
+    console.log('berhasil menembah ' + dokRef.id);
+  } catch (e) {
+    console.log('gagal menambah ' + e);
+  }
+}
+
+export async function hapusAbsensi(docId) {
+  await deleteDoc(doc(db, "absensi", docId));
+}
+
+export async function ubahPembeli(docId, nama, alamat, noTlpn) {
+  await updateDoc(doc(db, "pembeli", docId), {
+    nama: nama,
+    alamat: alamat,
+    noTlpn: noTlpn
+  });
+}
+
+export async function ambilPembeli(docId) {
+  const docRef = await doc(db, "pembeli", docId);
+  const docSnap = await getDoc(docRef);
+
+  return await docSnap.data(); 
+}
