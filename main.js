2531d7e19c7241fc4a90a8e4b93bf05a0d7e8558
svia435 <svia435@gmail.com>
2024-07-24 09:37:01 +0700

    update

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ export async function ambilDaftarAbsensi() {
       noTlpn: dok.data().noTlpn,
       kelas: dok.data().kelas,
       keterangan: dok.data().keterangan,
-      
+
     });
   });
 
@@ -75,17 +75,21 @@ export async function hapusAbsensi(docId) {
   await deleteDoc(doc(db, "absensi", docId));
 }
 
-export async function ubahPembeli(docId, nama, alamat, noTlpn) {
-  await updateDoc(doc(db, "pembeli", docId), {
+export async function ubahAbsensi(docId, tanggal, nis, nama, alamat, noTlpn, kelas, keterangan) {
+  await updateDoc(doc(db, "absensi", docId), {
+    tanggal: tanggal,
+    nis: nis,
     nama: nama,
     alamat: alamat,
-    noTlpn: noTlpn
+    noTlpn: noTlpn,
+    kelas: kelas,
+    keterangan: keterangan
   });
 }
 
-export async function ambilPembeli(docId) {
-  const docRef = await doc(db, "pembeli", docId);
+export async function ambilAbsensi(docId) {
+  const docRef = await doc(db, "absensi", docId);
   const docSnap = await getDoc(docRef);
 
-  return await docSnap.data(); 
+  return await docSnap.data();
 }
